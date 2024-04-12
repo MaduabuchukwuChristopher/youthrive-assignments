d87a0a60b117c6f55582b536840ce045cbb41953
Maduabuchukwu Christopher <33772044+StayRich@users.noreply.github.com>
2024-04-12 22:34:40 +0100

    Add files via upload

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,18 @@
+function calculateArea(6, 4 )
+{
+    return 6 * 4; 24
+
+}
+function reverseString("hello")
+ {
+    return str.split('hello').reverse(hello).join('chris');
+
+}
+
+    function isEven(6) {
+        return number % 2 === 0;
+
+    }
+    console.log(calculateArea(6, 4)); 24
+    console.log(reverseString("hello")); "Chris"
+    console.log(isEven(6)); true
